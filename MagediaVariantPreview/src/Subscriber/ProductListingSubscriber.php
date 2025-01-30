98291b4dd065580335dd43ee74b8591c7d295038
Vladislav-Matsiushenko <vladislav.matsiushenko@softera.by>
2025-01-30 14:15:47 +0300

    Added property group select

--- MagediaVariantPreview/src/Subscriber/ProductListingSubscriber.php
+++ MagediaVariantPreview/src/Subscriber/ProductListingSubscriber.php
@@ -8,6 +8,7 @@ use Shopware\Core\Content\Product\Events\ProductListingResultEvent;
 use Shopware\Core\Framework\DataAbstractionLayer\Search\Criteria;
 use Shopware\Core\Framework\DataAbstractionLayer\Search\Filter\EqualsFilter;
 use Shopware\Core\Framework\Struct\ArrayStruct;
+use Shopware\Core\System\SystemConfig\SystemConfigService;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Routing\RouterInterface;
@@ -16,6 +17,7 @@ use Symfony\Contracts\EventDispatcher\Event;
 class ProductListingSubscriber implements EventSubscriberInterface
 {
     public function __construct(
+        private readonly SystemConfigService $systemConfigService,
         private readonly EntityRepository $productRepository,
         private readonly RouterInterface $router
     ) {
@@ -41,9 +43,13 @@ class ProductListingSubscriber implements EventSubscriberInterface
 
     private function addVariantThumbnails(Event $event): void
     {
+        $propertyGroupId = $this->systemConfigService->get('MagediaVariantPreview.config.propertyGroupId');
+        if (!$propertyGroupId) {
+            return;
+        }
+
         $result = $event->getResult();
         $context = $event->getContext();
-
         foreach ($result->getEntities() as $product) {
             $parentId = $product->getChildCount() > 1 ? $product->getId() : $product->getParentId();
 
@@ -51,19 +57,33 @@ class ProductListingSubscriber implements EventSubscriberInterface
                 $criteria = new Criteria();
                 $criteria
                     ->addFilter(new EqualsFilter('product.parentId', $parentId))
+                    ->addAssociation('options.group')
                     ->addAssociation('media');
 
                 $variants = [];
+                $optionNames = [];
                 foreach ($this->productRepository->search($criteria, $context)->getEntities() as $child) {
-                    $variants[] = [
-                        'url' => $this->router->generate(
-                            'frontend.detail.page',
-                            ['productId' => $child->getId()],
-                            UrlGeneratorInterface::ABSOLUTE_URL
-                        ),
-                        'image' => $child->getMedia()?->first()?->getMedia()?->getUrl() ?? '',
-                        'name' => $child->getTranslation('name') ?? '',
-                    ];
+                    foreach ($child->getOptions() as $option) {
+                        if ($option->getGroup()?->getId() === $propertyGroupId) {
+
+                            $optionName = $option->getTranslation('name');
+                            if (!in_array($optionName, $optionNames)) {
+                                $optionNames[] = $optionName;
+
+                                $variants[] = [
+                                    'url' => $this->router->generate(
+                                        'frontend.detail.page',
+                                        ['productId' => $child->getId()],
+                                        UrlGeneratorInterface::ABSOLUTE_URL
+                                    ),
+                                    'image' => $child->getMedia()?->first()?->getMedia()?->getUrl() ?? '',
+                                    'name' => $child->getTranslation('name') ?? '',
+                                ];
+
+                                break;
+                            }
+                        }
+                    }
                 }
 
                 if (count($variants)) {
